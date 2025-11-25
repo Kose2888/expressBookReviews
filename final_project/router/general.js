d6ef21fc8afed6655ec8cf23cba4895c6920711d
kose2888 <ethan.kostiuk@gmail.com>
2025-11-25 13:54:19 -0700

    Can get book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,9 +28,19 @@ public_users.get('/', function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+
+  let book = Object.values(books).filter(book => {
+    return book.isbn === isbn;
+  });
+
+  if (book.length > 0) {
+    res.send(JSON.stringify(book, null, 4));
+  } else {
+    res.status(400).json({ message: "The book with the isbn " + isbn + " does not exist." });
+  }
+
  });
   
 // Get book details based on author
