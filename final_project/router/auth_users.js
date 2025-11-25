c1d9c9527e03ff2504b10568a8a1d357041d1e38
kose2888 <ethan.kostiuk@gmail.com>
2025-11-25 12:04:08 -0700

    Registered users can be authenticated using JWT and logged in

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,54 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => {
+  users.forEach(user => {
+    if (user.username === username) {
+      return true;
+    };
+    return false;
+  })
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => {
+  // Filter array for user with same username and password
+  let validusers = users.filter(user => {
+    return (user.username === username && user.password === password);
+  })
+  if (validusers.length > 0) {
+    return true; // Found a user
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if username or password is missing
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  // Authenticate user
+  if (authenticatedUser(username, password)) {
+    // Generate JWT access token
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 }); // access Token expires in 1 hour
+
+    // Store access and token username in session
+    req.session.authorization = {
+      accessToken, username
+    }
+    return res.status(200).send("User successfully loggen in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and passsword." });
+  }
+
+
 });
 
 // Add a book review
